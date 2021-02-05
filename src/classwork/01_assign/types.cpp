137950de325252a44e138a5403d614c8cc6edcc8
kevinmembrenoacc <jair.membreno@g.austincc.edu>
2021-02-04 21:26:26 -0600

    completed assignment1

--- src/classwork/01_assign/types.cpp
+++ src/classwork/01_assign/types.cpp
@@ -7,8 +7,11 @@ int sum_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
+/*Write function code, create an int variable named num2, assign it the
+ value 5, and multiply num1 and num2, and return their product. */
+int multiply_numbers(int num1)
+{
+	int num2 = 5; // int var
 
-
-
-
-
+	return num1 * num2;
+}
