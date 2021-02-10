e17a3185e606a18f24da75fc4cd03a8437ad944d
kevinmembrenoacc <jair.membreno@g.austincc.edu>
2021-02-10 17:14:03 -0600

    made changes to reflect class edits

--- src/examples/02_module/03_overflow/overflow.cpp
+++ src/examples/02_module/03_overflow/overflow.cpp
@@ -1,7 +1,23 @@
+#include "overflow.h"
+#include <climits>
 /*
 Function int_overflow creates an int num variable with value 2147483647, 
 add 1 to it, and returns it
-
 @param none
 @return num + 1
 */
+int int_overflow()
+{
+    auto num = 2147483647;
+    num = num + 1;
+
+    return num;
+}
+
+double double_underflow()
+{
+    auto closesToZeroDouble = __DBL_MIN__;
+    auto tooCloseToZeroDouble = closesToZeroDouble / 10;
+
+    return tooCloseToZeroDouble;
+}
