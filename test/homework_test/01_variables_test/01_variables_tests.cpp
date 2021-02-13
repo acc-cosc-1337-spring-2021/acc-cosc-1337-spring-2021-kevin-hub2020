b64f7b983d9eae3e26441695a7d6ca961937bc9a
kevinmembrenoacc <jair.membreno@g.austincc.edu>
2021-02-12 19:39:08 -0600

    completed homework 2, (I almost cried)

--- test/homework_test/01_variables_test/01_variables_tests.cpp
+++ test/homework_test/01_variables_test/01_variables_tests.cpp
@@ -12,3 +12,18 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+//Verify get_sales_tax_amount with parameter 10 returns the value .675
+//Verify get_sales_tax_amount with parameter 20 returns the value 1.35
+TEST_CASE("Test Get Sales Tax.")
+{
+	REQUIRE(get_sales_tax_amount(10) == 0.675);
+	REQUIRE(get_sales_tax_amount(20) == 1.35);
+}
+
+//Verify get_tip_amount with parameter 20 and 15% returns the value 3
+//Verify get_tip_amount with parameter 20 and 20% returns the value 4
+TEST_CASE("Test Get Tip Amount.")
+{
+	REQUIRE(get_tip_amount(20, 0.15) == 3);
+	REQUIRE(get_tip_amount(20, 0.20) == 4);
+}
