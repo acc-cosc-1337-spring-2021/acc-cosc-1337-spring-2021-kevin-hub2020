52a99323ec0242a3eae81c5f205f1ee393733b51
kevinmembrenoacc <jair.membreno@g.austincc.edu>
2021-03-04 16:56:26 -0600

    Homework 4

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,13 @@
 //cpp
+#include "loops.h"
+using namespace std;
+
+int factorial (int num)
+{
+    int factorial = num;
+    for (int i = num - 1; i > 0; i--)
+    {
+        factorial = factorial * i;
+    }
+    return factorial;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,9 @@
 //write includes statements
-
+#include "loops.h"
+#include <iostream>
 //write using statements for cin and cout
-
+using std::cout;
+using std::cin;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +12,28 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	char choice;
+	int num;
 
+	do
+	{
+		do
+		{
+			// Other input thats not an integer will cause an infinite loop and freeze VSC, scary....
+			cout<<"\nEnter a positive number as an integer: ";
+			cin>>num;
+
+			while ((num <=0) || (num > 10))
+			{
+				cout<<"\nError! Input must be a positive integer between 1 to 10";
+				cout<<"\nEnter a positive number as an integer: ";
+				cin>>num;
+			}
+		} while (num <= 0 || num > 10);
+		cout<<"The Factorial of "<<num<<" is: "<<factorial(num);
+
+		cout<<"\nEnter a new number again? (Y/N): ";
+		cin>>choice;
+	} while (toupper(choice) == 'Y');
 	return 0;
 }
