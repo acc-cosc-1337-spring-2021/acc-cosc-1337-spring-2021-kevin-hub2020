605d4aa507e7cab929bf7bd20f855f0e444f4d3b
kevinmembrenoacc <jair.membreno@g.austincc.edu>
2021-03-17 20:04:33 -0500

    04 module, 06 vectors

--- src/examples/04_module/06_vectors/main.cpp
+++ src/examples/04_module/06_vectors/main.cpp
@@ -1,7 +1,25 @@
 #include "vec.h"
+#include <string>
+using std::string;
+
+using std::vector;
+using std::cout;
 
 int main() 
 {
+	vector<string> names{"joe", "mary", "john"};
+	names[1] = "jane";
+	// vector<int> nums(10, 10);
+	// nums.push_back(10);
+	// nums.push_back(3);
+	// nums.push_back(2);
+
+	// loop_vector_w_for_ranged(nums);
+
+	for (auto name: names)
+	{
+		cout<<name<<"\n";
+	}
 
 	return 0;
 }
--- src/examples/04_module/06_vectors/vec.h
+++ src/examples/04_module/06_vectors/vec.h
@@ -1,26 +1,28 @@
 #include<string>
 #include<vector>
+#include<iostream>
 
 /*
 Write function prototype for void function loop_vector_w_index with no parameters.
-
 */
 
 
 /*
 Write function prototype for void function loop_vector_w_index_val with a vector of int value parameter.
 */
-
+void loop_vector_w_index_val(std::vector<int> nums);
 
 
 /*
 Write function prototype for void function loop_vector_w_index_ref with a vector of int reference parameter.
 */
-
+void loop_vector_w_index_ref(std::vector<int>& nums);
 
 /*
 Write function prototype for void function loop_vector_w_index_const_ref with a vector of int const reference parameter.
 */
 
+std::vector<double> get_vector();
 
-
+void loop_vector_w_for_ranged(std::vector<int> nums);
+void loop_vector_w_for_ranged_ref(std::vector<int>& nums);
