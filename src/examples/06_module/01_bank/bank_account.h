911a7c6ae2c8b4cc9ecce8868e161e109a70ff32
kevinmembrenoacc <jair.membreno@g.austincc.edu>
2021-03-24 21:37:45 -0500

    intro to object oriented stuff

--- src/examples/06_module/01_bank/bank_account.h
+++ src/examples/06_module/01_bank/bank_account.h
@@ -1,1 +1,9 @@
 //bank_account.h
+class BankAccount
+{
+public://access specifier
+    int get_balance()const {return balance;} //const protects get_balance()
+private:
+    int balance{0};
+
+};
--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -1,6 +1,11 @@
+#include<iostream>
+using std::cout;
+#include "bank_account.h"
 
 int main()
 {
+	BankAccount account;//variable - an instance of BankAccount object
+	cout<<"Balance: "<<account.get_balance()<<"\n";
 
 	return 0;
 }
