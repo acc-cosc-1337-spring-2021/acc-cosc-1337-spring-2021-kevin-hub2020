16f43b00f32beca0aeb2d0ca4d705c89000536cd
kevinmembrenoacc <jair.membreno@g.austincc.edu>
2021-04-12 14:39:56 -0500

    module 6, 01 bank

--- src/examples/06_module/01_bank/bank_account.cpp
+++ src/examples/06_module/01_bank/bank_account.cpp
@@ -6,6 +6,7 @@ void BankAccount::deposit(int amount)
     if(amount > 0)
     {
         balance += amount;
+        bank_balance += amount;
     }
 }
 
@@ -14,7 +15,39 @@ void BankAccount::withdraw(int amount)
     if(amount > 0 && amount <= balance)
     {
         balance -= amount;
+        bank_balance -= amount;
     }        
+}
+
+int BankAccount::bank_balance = 0;
+
+//FRIEND FREE FUNCTION
+void friend_display_balance(const BankAccount& account)
+{
+    std::cout<<"Friend Display balance: "<<account.balance<<"\n";
+}
+
+std::ostream& operator<<(std::ostream& out, const BankAccount& account)
+{
+    out<<"Overloaded ostream Display balance: "<<account.balance<<"\n"; 
+
+    return out;   
+}
+
+std::istream& operator>>(std::istream& in, BankAccount& account)
+{
+    int amount = 0;
+    std::cout<<"Enter amount: ";
+    in>>amount;
+    account.balance += amount;
+
+    return in; 
+}
+
+BankAccount operator+(const BankAccount& a1, const BankAccount& a2)
+{
+    BankAccount account(a1.balance + a2.balance);
+    return account;
 }
 
 //FREE FUNCTIONS NOT PART OF THE BANK ACCOUNT CLASS!!!!!!
@@ -28,4 +61,12 @@ BankAccount get_account(int balance)
     BankAccount account(balance);
 
     return account;
+}
+
+
+//======branch bank class
+void BranchBank::update_balance(int b)
+{
+    branch_balance += b;
+    BankAccount::bank_balance += b;
 }
--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -1,29 +1,49 @@
-#include "bank_account.h"
 #include<iostream>
+#include<time.h>
 using std::cout;
+using std::cin;
 
+#include "atm.h"
+#include "bank_account.h"
 
 int main()
 {
+	// string name;// creates a string var in memory
+	// 			//create an instance of an object
+
+	srand(time(NULL));
 	BankAccount account = get_account(100);
-
-	display_balance(account);
+	BankAccount a1(100);
+	BankAccount a2(200);
+	BankAccount a3 = a1 + a2;//get_account(100);//variable-an instance of BankAccount object
+	cout<<a3;
+	//	display_balance(account);
 	
-//	cout<<sizeof(account)<<"\n";
+	/*char choice;
+	ATM atm;
+	/*char choice;
+	
+	do
+	{
+		
+		atm.scan_card();
+		atm.display_balance();
+		cout<<"Continue? y or n";
+		cin>>choice;
+	}while(choice == 'y' || choice == 'Y');*/
+
+	/*cout<<sizeof(account)<<"\n";
 	auto balance = account.get_balance();
-
 	cout<<"Balance: "<<balance<<"\n";
-
 	cout<<"Deposit $50\n";
 	account.deposit(50);
-
 	balance = account.get_balance();
 	cout<<"Balance: "<<balance<<"\n\n";
-
 	cout<<"Withdraw $10\n";
 	account.withdraw(10);
 	balance = account.get_balance();
-	cout<<"Balance: "<<balance<<"\n";
+	cout<<"Balance: "<<balance<<"\n";*/
 
+	
 	return 0;
 }
