a9a5548566b6509221740740f4b54aa9f06ddf03
kevinmembrenoacc <jair.membreno@g.austincc.edu>
2021-04-22 18:43:50 -0500

    Inheritance ptrs, Polymorphism, Abstract class

--- test/examples_test/06_module_test/06_module_tests.cpp
+++ test/examples_test/06_module_test/06_module_tests.cpp
@@ -1,110 +1,126 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "bank_account.h"
+#include "savings_account.h"
+#include "checking_account.h"
+#include<memory>
+using std::unique_ptr; using std::make_unique;
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-TEST_CASE("Test new bank account object balance")
-{
-	BankAccount account;
-
-	REQUIRE(account.get_balance() == 0);
-}
-
-TEST_CASE("Test new bank account with given begin balance")
-{
-	BankAccount account(100);
-
-	REQUIRE(account.get_balance() == 100);
-}
-
-TEST_CASE("Test bank account deposit")
-{
-	BankAccount account;
-	account.deposit(50);
-
-	REQUIRE(account.get_balance() == 50);
-
-}
-
-TEST_CASE("Test bank account deposit with given begin balance")
-{
-	BankAccount account(100);
-
-	account.deposit(50);
-	REQUIRE(account.get_balance() == 150);
-}
-
-TEST_CASE("Test bank account deposit with negative amount")
-{
-	BankAccount account;
-	account.deposit(50);
-	REQUIRE(account.get_balance() == 50);
-
-	account.deposit(-10);
-	REQUIRE(account.get_balance() == 50);
-}
-
-TEST_CASE("Test bank account withdraw")
-{
-	BankAccount account;
-	account.deposit(50);
-	REQUIRE(account.get_balance() == 50);
-
-	account.withdraw(10);
-	REQUIRE(account.get_balance() == 40);
-}
-
-TEST_CASE("Test bank account withdraw with a given begin balance")
-{
-	BankAccount account(100);
-	account.withdraw(10);
-
-	REQUIRE(account.get_balance() == 90);
-}
-
-TEST_CASE("Test Bank account withdraw with negative amount")
-{
-	BankAccount account;
-	account.deposit(50);
-	REQUIRE(account.get_balance() == 50);
-
-	account.withdraw(-10);
-	REQUIRE(account.get_balance() == 50);
-}
-
-TEST_CASE("Test Bank account withdraw with NSF")
-{
-	BankAccount account;
-	account.deposit(50);
-	REQUIRE(account.get_balance() == 50);
-
-	account.withdraw(51);
-	REQUIRE(account.get_balance() == 50);
-}
-
 TEST_CASE("Test bank account bank_balance")
 {
-	BankAccount account;
-	account.deposit(50);
+	unique_ptr<BankAccount>  account = make_unique<SavingsAcount>();
+	account->deposit(50);
 
-	REQUIRE(account.get_bank_balance() == 50);
+	REQUIRE(account->get_bank_balance() == 50);
+}
+
+TEST_CASE("Test new bank account balance is 0")
+{
+	unique_ptr<BankAccount>  account = make_unique<SavingsAcount>();
+
+	REQUIRE(account->get_balance() == 0);
+}
+
+TEST_CASE("Test bank account deposit with valid amount")
+{
+	unique_ptr<BankAccount>  account = make_unique<CheckingAccount>();
+	REQUIRE(account->get_balance() == 0);
+	
+	account->deposit(50);
+	REQUIRE(account->get_balance() == 50);
+}
+
+TEST_CASE("Test bank account deposit with invalid amount")
+{
+	unique_ptr<BankAccount>  account = make_unique<CheckingAccount>();
+	REQUIRE(account->get_balance() == 0);
+	
+	account->deposit(50);
+	REQUIRE(account->get_balance() == 50);
+
+	account->deposit(-10);
+	REQUIRE(account->get_balance() == 50);
+}
+
+TEST_CASE("Test bank account withdraw with valid amount")
+{
+	unique_ptr<BankAccount>  account = make_unique<SavingsAcount>();
+	REQUIRE(account->get_balance() == 0);
+	
+	account->deposit(50);
+	REQUIRE(account->get_balance() == 50);	
+
+	account->withdraw(10);
+	REQUIRE(account->get_balance() == 40);	
+}
+
+TEST_CASE("Test bank account withdraw with amount less than 0")
+{
+	unique_ptr<BankAccount>  account = make_unique<SavingsAcount>();
+	REQUIRE(account->get_balance() == 0);
+	
+	account->deposit(50);
+	REQUIRE(account->get_balance() == 50);	
+
+	account->withdraw(-10);
+	REQUIRE(account->get_balance() == 50);	
+}
+
+TEST_CASE("Test bank account withdraw with amount greater than balance")
+{
+	unique_ptr<BankAccount>  account = make_unique<SavingsAcount>();
+	REQUIRE(account->get_balance() == 0);
+	
+	account->deposit(50);
+	REQUIRE(account->get_balance() == 50);	
+	
+	account->withdraw(51);
+	REQUIRE(account->get_balance() == 50);	
+}
+
+TEST_CASE("Test new bank account balance is 100")
+{
+	unique_ptr<BankAccount>  account = make_unique<CheckingAccount>();
+
+	REQUIRE(account->get_balance() == 100);
+}
+
+TEST_CASE("Test bank account deposit with begin balance 100")
+{
+	unique_ptr<BankAccount>  account = make_unique<CheckingAccount>();
+
+	REQUIRE(account->get_balance() == 100);
+
+	account->deposit(50);
+	REQUIRE(account->get_balance() == 150);
+}
+
+TEST_CASE("Test bank account withdraw with begin balance 100")
+{
+	unique_ptr<BankAccount>  account = make_unique<SavingsAcount>();
+
+	REQUIRE(account->get_balance() == 100);
+
+	account->withdraw(50);
+	REQUIRE(account->get_balance() == 50);
 }
 
 TEST_CASE("Test bank account bank_balance with deposit")
 {
-	BankAccount account;
-	account.deposit(50);
+	unique_ptr<BankAccount>  account = make_unique<CheckingAccount>();
+	account->deposit(50);
 
-	REQUIRE(account.get_bank_balance() == 640);
+	REQUIRE(account->get_bank_balance() == 640);
 
 }
 
 TEST_CASE("Test branch bank class")
 {
-	BankAccount account;
+	unique_ptr<BankAccount>  account = make_unique<SavingsAcount>();
 	BranchBank branch_bank(100000);
 
 	REQUIRE(branch_bank.get_branch_balance() == 100000);
@@ -112,6 +128,6 @@ TEST_CASE("Test branch bank class")
 	branch_bank.update_balance(5000);
 	REQUIRE(branch_bank.get_branch_balance() == 105000);
 
-	account.deposit(100);
-	REQUIRE(account.get_bank_balance() == 5740);
+	account->deposit(100);
+	REQUIRE(account->get_bank_balance() == 5740);
 }
